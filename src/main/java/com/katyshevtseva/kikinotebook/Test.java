6c1c5be6ae55466ad29eed86136efc38fcb49abf
EkaterinaKat <katyshevtseva_ekaterina@mail.ru>
2025-01-16 14:04:35 +0500

    tests

--- src/main/java/com/katyshevtseva/kikinotebook/Test.java
+++ src/main/java/com/katyshevtseva/kikinotebook/Test.java
@@ -1,14 +1,8 @@
 package com.katyshevtseva.kikinotebook;
 
-import com.katyshevtseva.kikinotebook.core.films.web.FilmSearchEngine;
-
 public class Test {
 
     public static void main(String[] args) {
-        try {
-            System.out.println(FilmSearchEngine.findFilms("Леон"));
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+
     }
 }
---
+++ src/main/java/com/katyshevtseva/kikinotebook/core/Tests.java
@@ -0,0 +1,49 @@
+package com.katyshevtseva.kikinotebook.core;
+
+import com.katyshevtseva.kikinotebook.core.films.FilmsService;
+import com.katyshevtseva.kikinotebook.core.films.PosterFileManager;
+import com.katyshevtseva.kikinotebook.core.films.model.Film;
+import com.katyshevtseva.kikinotebook.core.films.model.FilmToWatch;
+import com.katyshevtseva.kikinotebook.core.films.model.PosterState;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Tests {
+
+    public static void testPosterState() {
+        for (Film film : FilmsService.getAllFilms()) {
+            boolean a = PosterFileManager.filmHasPoster(film);
+            boolean b = film.getPosterState() == PosterState.LOADED;
+            if (a != b) {
+                System.out.println("error!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+            } else {
+                System.out.println("it's fine");
+            }
+        }
+    }
+
+    public static void testKpIdUniqueness() {
+        List<Film> films = Dao.getAllFilms();
+        List<FilmToWatch> filmsToWatch = Dao.getAllFilmsToWatch();
+        List<Long> idList = new ArrayList<>();
+        idList.addAll(films.stream().map(Film::getKpId).collect(Collectors.toList()));
+        idList.addAll(filmsToWatch.stream().map(FilmToWatch::getKpId).collect(Collectors.toList()));
+        idList = idList
+                .stream()
+                .filter(Objects::nonNull)
+                .sorted()
+                .collect(Collectors.toList());
+
+        for (int i = 0; i < idList.size() - 1; i++) {
+            long a = idList.get(i);
+            long b = idList.get(i + 1);
+            if (a == b) {
+                System.out.println(a);
+            }
+        }
+
+        Set<Long> idSet = new HashSet<>(idList);
+        System.out.println("Id list has duplicates: " + (idSet.size() < idList.size()));
+    }
+}
